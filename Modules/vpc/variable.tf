a9512b16dab3c4b92887d4c3d17bd622200cdaa0
iam-amolgosavi <149919930+iam-amolgosavi@users.noreply.github.com>
2025-07-10 20:22:28 +0530

    Create variable.tf

---
+++ Modules/vpc/variable.tf
@@ -0,0 +1,25 @@
+# variables.tf
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources in."
+  type        = string
+  default     = "eu-west-1" # Or your preferred region
+}
+
+variable "project_name" {
+  description = "A unique name for your project, used as a prefix for resources."
+  type        = string
+  default     = "my-eks-app"
+}
+
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC."
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "eks_kubernetes_version" {
+  description = "The Kubernetes version for the EKS cluster."
+  type        = string
+  default     = "1.28" # Check AWS EKS supported versions
+}
